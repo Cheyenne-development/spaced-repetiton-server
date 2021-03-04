c871e5cde30ed420645263a43ff338e657b2f4f9
Cheyenne McMahon <orlandu8@gmail.com>
2021-03-04 04:16:33 -0600

    first commit

---
+++ seeds/seed.tables.sql
@@ -0,0 +1,41 @@
+BEGIN;
+
+TRUNCATE
+  "word",
+  "language",
+  "user";
+
+INSERT INTO "user" ("id", "username", "name", "password")
+VALUES
+  (
+    1,
+    'admin',
+    'Dunder Mifflin Admin',
+    -- password = "pass"
+    '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
+  );
+
+INSERT INTO "language" ("id", "name", "user_id")
+VALUES
+  (1, 'Spanish', 1);
+
+INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
+VALUES
+  (1, 1, 'ciervo', 'deer', 2),
+  (2, 1, 'hola', 'hello', 3),
+  (3, 1, 'casa', 'house', 4),
+  (4, 1, 'mercado', 'market', 5),
+  (5, 1, 'vaca', 'cow', 6),
+  (6, 1, 'ordenador', 'computer', 7),
+  (7, 1, 'perro', 'dog', 8),
+  (8, 1, 'gato', 'cat', null);
+
+UPDATE "language" SET head = 1 WHERE id = 1;
+
+-- because we explicitly set the id fields
+-- update the sequencer for future automatic id setting
+SELECT setval('word_id_seq', (SELECT MAX(id) from "word"));
+SELECT setval('language_id_seq', (SELECT MAX(id) from "language"));
+SELECT setval('user_id_seq', (SELECT MAX(id) from "user"));
+
+COMMIT;
---
+++ src/language/language-service.js
@@ -0,0 +1,112 @@
+const LinkedList = require('../linked-list/LinkedList');
+
+const LanguageService = {
+  getUsersLanguage(db, user_id) {
+    return db
+      .from('language')
+      .select(
+        'language.id',
+        'language.name',
+        'language.user_id',
+        'language.head',
+        'language.total_score'
+      )
+      .where('language.user_id', user_id)
+      .first();
+  },
+
+  getLanguageWords(db, language_id) {
+    return db
+      .from('word')
+      .select(
+        'id',
+        'language_id',
+        'original',
+        'translation',
+        'next',
+        'memory_value',
+        'correct_count',
+        'incorrect_count'
+      )
+      .where({ language_id });
+  },
+
+  getNextWord(db, id) {
+    return db('word')
+      .select('id', 'next', 'original', 'correct_count', 'incorrect_count')
+      .where({ id })
+      .first();
+  },
+
+  populateWords(db, lang, words) {
+    const wordList = new LinkedList();
+    wordList.id = lang.id;
+    wordList.name = lang.name;
+    wordList.total_score = lang.total_score;
+
+    let headWord = words.find((word) => word.id === lang.head);
+
+    wordList.insertFirst({
+      id: headWord.id,
+      original: headWord.original,
+      translation: headWord.translation,
+      memory_value: headWord.memory_value,
+      correct_count: headWord.correct_count,
+      incorrect_count: headWord.incorrect_count,
+    });
+    while (headWord.next) {
+      headWord = words.find((word) => word.id === headWord.next);
+      wordList.insertLast({
+        id: headWord.id,
+        original: headWord.original,
+        translation: headWord.translation,
+        memory_value: headWord.memory_value,
+        correct_count: headWord.correct_count,
+        incorrect_count: headWord.incorrect_count,
+      });
+    }
+    return wordList;
+  },
+
+  updateWord(db, word) {
+    return db.from('word').where({ id: word.id }).update({
+      memory_value: word.memory_value,
+      correct_count: word.correct_count,
+      incorrect_count: word.incorrect_count,
+    });
+  },
+
+  incrementTotal(db, lang) {
+    return db
+      .from('lang')
+      .where({ id: lang.id })
+      .update({
+        total_score: lang.total_score + 1,
+      });
+  },
+
+  persistLL(db, lang) {
+    return db.transaction((trx) =>
+      Promise.all([
+        db('language').transacting(trx).where('id', lang.id).update({
+          total_score: lang.total_score,
+          head: lang.head.value.id,
+        }),
+
+        ...lang.forEach((node) =>
+          db('word')
+            .transacting(trx)
+            .where('id', node.value.id)
+            .update({
+              memory_value: node.value.memory_value,
+              correct_count: node.value.correct_count,
+              incorrect_count: node.value.incorrect_count,
+              next: node.next ? node.next.value.id : null,
+            })
+        ),
+      ])
+    );
+  },
+};
+
+module.exports = LanguageService;
